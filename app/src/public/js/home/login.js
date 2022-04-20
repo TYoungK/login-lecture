c88dd9d4870f822fb091d879cc9353db3573b8fb
TYoungK <xordud525@nate.com>
2022-04-20 20:37:18 +0900

    modify login.js

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 'use strict'
 
-console.log('hello');
+const id = document.querySelector('#id'),
+    pwd = document.querySelector('#pwd'),
+    loginBtn = document.querySelector('button');
+
+
+loginBtn.addEventListener('click', )
+function login(){
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+}
